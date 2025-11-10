6a3b03d47b337c63b7dd5f299115cc85e5841e77
fnrir <fnr1r0@protonmail.com>
2025-11-10 19:44:32 +0100

    feat: add dev hello example

---
+++ examples/hello.rs
@@ -0,0 +1,18 @@
+use anyhow::Result;
+use cros_ec::{
+    cmds::{hello::ec_cmd_hello, proto_version::ec_cmd_proto_version},
+    interfaces::EcDev,
+};
+
+fn main() -> Result<()> {
+    let ec_dev = EcDev::open_cros_ec()?;
+    eprintln!("EC opened!");
+
+    ec_cmd_hello(&ec_dev)?;
+    eprintln!("EC Hello successful!");
+
+    let proto_version = ec_cmd_proto_version(&ec_dev)?;
+    eprintln!("EC Protocol Version: 0x{:08x}", proto_version);
+
+    Ok(())
+}
