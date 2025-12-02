ee47096266f7069bb2bbf7ec957575459593b36a
fnrir <fnr1r0@protonmail.com>
2025-12-02 10:50:57 +0100

    feat: add some methods on VersionMask

--- src/types/version_mask.rs
+++ src/types/version_mask.rs
@@ -6,3 +6,16 @@ use plain::Plain;
 pub struct VersionMask(pub u32);
 
 unsafe impl Plain for VersionMask {}
+
+impl VersionMask {
+    pub const BITS: u8 = u32::BITS as u8;
+    pub const fn new(mask: u32) -> Self {
+        Self(mask)
+    }
+    pub const fn empty() -> Self {
+        Self(0)
+    }
+    pub const fn is_supported(&self, version: u8) -> bool {
+        self.0 & 1 << version != 0
+    }
+}
