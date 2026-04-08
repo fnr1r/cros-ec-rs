131d7187581e6e7e2ed100f21b89a8f34e0e1552
fnrir <fnr1r0@protonmail.com>
2026-04-08 16:07:13 +0200

    feat(pwm): add pwm commands to enum

--- src/types/command.rs
+++ src/types/command.rs
@@ -13,6 +13,15 @@ pub enum EcKnownCommand {
     GetCmdVersions = 0x0008,
     GetFeatures = 0x000D,
     FlashInfo = 0x0010,
+    PwmGetFanTargetRpm = 0x0020,
+    PwmSetFanTargetRpm = 0x0021,
+    /// OBSOLETE - Use [`PwmGetDuty`](Self::PwmGetDuty)
+    PwmGetKeyboardBacklight = 0x0022,
+    /// OBSOLETE - Use [`PwmSetDuty`](Self::PwmSetDuty)
+    PwmSetKeyboardBacklight = 0x0023,
+    PwmSetFanDuty = 0x0024,
+    PwmSetDuty = 0x0025,
+    PwmGetDuty = 0x0026,
     FwChargeLimit = 0x3E03,
 }
 
